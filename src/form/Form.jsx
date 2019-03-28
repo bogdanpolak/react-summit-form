e4c8597f3a9530eaa0779ac1f0b5670ec7a3ba74
Bogdan Polak <bogdan.polak@bsc.com.pl>
2019-03-28 16:11:03 +0100

    Usunięto komponent FormFieldEmail

--- src/form/Form.jsx
+++ src/form/Form.jsx
@@ -1,35 +1,11 @@
 import React, {Component} from "react";
 import PropTypes from 'prop-types';
+
 import FormFieldText from "./FieldText.jsx";
-import FormFieldEmail from "./FieldEmail.jsx";
 import FieldExtTickets from "./FieldExtTickets.jsx";
 import FieldAgreeGDPR from "./FieldAgreeGDPR.jsx";
 
-
 export default class Form extends Component{
-
-  renderFormField (field) {
-    var fd;
-    switch (field.fieldType) {
-      case "text":
-        fd = <FormFieldText field={field}/>
-        break
-      case "email":
-        fd = <FormFieldEmail field={field}/>
-        break
-    }
-    return fd
-  }
-  
-  renderTwoColumns(fields) {
-    return fields.map ( 
-      (fld,idx) => (
-        <div className="col-sm-6" key={idx.toString()}>
-          {this.renderFormField (fld)}
-        </div> 
-      )
-    )
-  }
 
   render() {
     var rows = this.props.formModel.map((row,idx)=> {
@@ -44,7 +20,21 @@ export default class Form extends Component{
         case "two-columns":
           return (
             <div key={key} className="form-row">
-              {this.renderTwoColumns(row.fields)}
+              {
+                row.fields.map ( (field,idx)=> (
+                  <div 
+                    key={idx.toString()}
+                    className="col-sm-6" >
+                    <FormFieldText 
+                      type = {field.fieldType}
+                      id = {field.name}
+                      caption = {field.caption}
+                      feedback = {field.feedback}
+                      isRequired = {field.isRequired}
+                    />
+                  </div> 
+                ))
+              }
             </div>
           )
         case "ext-tickets":
